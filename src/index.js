38a2b7da796188d3d6c992e42fac131a5d7bcde9
avramenkomy <avramenkomy@yandex.ru>
2021-08-29 00:00:50 +0300

    WIP: initial empty root reducer

--- src/index.js
+++ src/index.js
@@ -1,7 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { Provider } from "react-redux";
+import { createStore, compose, applyMiddleware } from "redux";
+import thunk from "redux-thunk";
+
+import App from './App';
+import { rootReducer } from './redux/rootReducer'
+
+const store = createStore(
+  rootReducer,
+  compose(
+    applyMiddleware(
+      thunk,
+    ),
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  )
+);
+
+const app = (
+  <Provider store={store}>
+    <App />
+  </Provider>
+)
 
 ReactDOM.render(
   <React.StrictMode>
